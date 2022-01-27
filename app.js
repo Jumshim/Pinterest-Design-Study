32522b9d4d35b0b5c7b1a5ee2d968a7bce0b2544
jeffmun <61306390+Jumshim@users.noreply.github.com>
2022-01-26 21:31:45 -0800

    more about flex

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const app = express();
 const port = 2000;
 
 app.set("view options", {layout: false});
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname) + "/views"));
+app.set('views', path.join(__dirname, '/views'));
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) {
     res.render('index.html');
 });
+
+app.get('/temp', function(req, res) {
+    console.log('Request for temp page received');
+    res.render('temp.html');
+})
 
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`);
