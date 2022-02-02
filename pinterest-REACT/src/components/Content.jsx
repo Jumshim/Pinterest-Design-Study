4154ad1099173f47d5e403af78de909e63e46eb1
jeffmun <61306390+Jumshim@users.noreply.github.com>
2022-02-01 22:47:19 -0800

    added for loop to pincolumn

--- pinterest-REACT/src/components/Content.jsx
+++ pinterest-REACT/src/components/Content.jsx
@@ -11,9 +11,6 @@ const pinColumnCss = css`
   margin: 0px 8px;
 `
 
-//put img links in express
-//use fetch to see client vs server rendering and fetching
-
 const bodyCss = css`
   display: flex;
   grid-area: body;
@@ -61,47 +58,21 @@ class Content extends React.Component {
   // five nines - represent 99.999%
   // the uptime that you want during the year. Servers are not resilient to network faults
   // problems with scaling upwards or horizontally
-  /**
-   * TODO
-   * can you make this more generic? A function, a better for loop, etc... a lot of repeated stuff, but we want to be
-   * good coders :)
-   * "dry" - don't repeat yourself
-   * common term in computer science / industry
-   * "you want to dry up your code after making it"
-   */
 
   /**
-   * images in a sqlite server and get data from that
-   * research pagination more
-   * how to implement pagination in this server?
-   * whatever we find interesting, we will do that :)
-   * @returns 
-   */
-
-  /**
-   * TODO: Pagination in a SQL query
-   * Reuse table design from last time
-   * Run the table schema by herk b4 implementing
    * for credit: cursor based
-   * @returns 
    */
   render(){
+    let columns = [];
+    for(let i = 0; i < 5; i++) {
+      columns.push(
+        <div css={pinColumnCss}>
+          {this.state.img.filter((img, index) => index%5===i).map(link => <PinColumn url={link.URL} key={link.URL}/> )}
+        </div>
+      )
+    }
     return <div css={bodyCss} onScroll={this.handleScroll}>
-      <div css={pinColumnCss}>
-        {this.state.img.filter((img, index) => index%5===0).map(link => <PinColumn url={link.URL} key={link.URL}/> )}
-      </div>
-      <div css={pinColumnCss}>
-        {this.state.img.filter((img, index) => index%5===1).map(link => <PinColumn url={link.URL} key={link.URL}/> )}
-      </div>
-      <div css={pinColumnCss}>
-        {this.state.img.filter((img, index) => index%5===2).map(link => <PinColumn url={link.URL} key={link.URL}/> )}
-      </div>
-      <div css={pinColumnCss}>
-        {this.state.img.filter((img, index) => index%5===3).map(link => <PinColumn url={link.URL} key={link.URL}/> )}
-      </div>
-      <div css={pinColumnCss}>
-        {this.state.img.filter((img, index) => index%5===4).map(link => <PinColumn url={link.URL} key={link.URL}/> )}
-      </div>
+      {columns};
     </div>
   }
 }
