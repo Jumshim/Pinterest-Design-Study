6ba7ecc177a9c542f622dd0af356a118cfa471c6
jeffmun <61306390+Jumshim@users.noreply.github.com>
2022-02-14 23:57:28 -0800

    routing stuff

--- pinterest-REACT/src/Account.jsx
+++ pinterest-REACT/src/Account.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import App from './App';
+import Header from './components/Header';
+
 const Account = () => {
     return (
         <div>
             <h1> Log In </h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/">
+                        <App></App>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 };
