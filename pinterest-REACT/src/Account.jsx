bf6d99968cb19614e75891b86bb22687e1d05dec
jeffmun <61306390+Jumshim@users.noreply.github.com>
2022-02-15 23:59:14 -0800

    hi

--- pinterest-REACT/src/Account.jsx
+++ pinterest-REACT/src/Account.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { jsx, css } from '@emotion/react'
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './App';
 import Header from './components/Header';
 
+const bodyCss = css`
+  display: flex;
+  grid-area: body;
+  flex-direction: row;
+  margin: 0px 80px;
+  justify-content: center;
+  overflow: scroll;
+  scrollbar-width: none;
+`;
+
+const LoginBox = () => {
+    return (
+        <h1> hi </h1>
+    );
+}
+
+
 const Account = () => {
     return (
-        <div>
+        <div css={bodyCss}>
             <h1> Log In </h1>
+            <p> aiodaoisdjaiosdjasiodjsaiodjasiodjaio </p>
         </div>
     );
 };
