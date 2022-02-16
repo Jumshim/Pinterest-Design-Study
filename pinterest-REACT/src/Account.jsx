d5d56d643b8843f46a155eb910d8bd752d28bbd2
jeffmun <61306390+Jumshim@users.noreply.github.com>
2022-02-16 10:12:54 -0800

    from physics class

--- pinterest-REACT/src/Account.jsx
+++ pinterest-REACT/src/Account.jsx
@@ -1,34 +1,42 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
 import React from 'react';
 import { jsx, css } from '@emotion/react'
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import App from './App';
-import Header from './components/Header';
-
 const bodyCss = css`
   display: flex;
   grid-area: body;
   flex-direction: row;
-  margin: 0px 80px;
+  margin: 80px 80px;
   justify-content: center;
   overflow: scroll;
   scrollbar-width: none;
 `;
 
+const loginCss = css`
+    height: 480px;
+    width: 400px;
+    border-radius: 30px;
+    justify-content: center;
+`;
+
+
 const LoginBox = () => {
     return (
-        <h1> hi </h1>
+        <div css={loginCss}>
+            <h1> Welcome Back! </h1>
+            <input type="text" placeholder="Username/Email" ></input>
+            <input type="text" placeholder="Password" ></input>
+        </div>
     );
 }
 
 
 const Account = () => {
-    return (
-        <div css={bodyCss}>
-            <h1> Log In </h1>
-            <p> aiodaoisdjaiosdjasiodjsaiodjasiodjaio </p>
-        </div>
+    return ( <LoginBox></LoginBox>
     );
 };
 
